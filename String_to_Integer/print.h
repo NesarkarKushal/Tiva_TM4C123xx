7aa1810c7c5f98601f25855b34538b73c31272be
NesarkarKushal <86586956+NesarkarKushal@users.noreply.github.com>
2022-02-11 09:38:34 +0530

    Converting String to Integer in Code Composer Studio

--- String_to_Integer/print.h
+++ String_to_Integer/print.h
@@ -9,12 +9,6 @@
 #define PRINT_H_
 
 extern void print_init();
-extern char *i_to_a(int num);
-extern int no_of_digits(int num);
-
 extern int stoa(char *str);  //string to Integer
-
-
-
 
 #endif /* PRINT_H_ */
