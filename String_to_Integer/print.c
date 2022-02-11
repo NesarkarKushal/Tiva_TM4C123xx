6031472041d9fe4bd3da77207610cced7faea069
NesarkarKushal <86586956+NesarkarKushal@users.noreply.github.com>
2022-02-11 09:37:00 +0530

    Converting String to Integer in Code Composer Studio

--- String_to_Integer/print.c
+++ String_to_Integer/print.c
@@ -36,55 +36,6 @@ void print_init()
 
 }
 
-
-
-
-
-
-
-int no_of_digits(int num)
-{
-    int digit_count = 0;
-
-    while(num > 0)
-    {
-        digit_count++;
-        num /= 10;
-    }
-
-    return digit_count;
-}
-char *i_to_a(int num)
-{
-    char *str;
-    int digit_count = 0;
-
-    if(num < 0)
-    {
-        num = -1*num;
-        digit_count++;
-    }
-
-    digit_count += no_of_digits(num);
-    str = malloc(sizeof(char)*(digit_count+1));
-
-    str[digit_count] = '\0';
-
-    while(num > 0)
-    {
-        str[digit_count-1] = num%10 + '0';
-        num = num/10;
-        digit_count--;
-    }
-
-    if(digit_count == 1)
-        str[0] = '-';
-
-    return str;
-}
-
-
-
 int stoa(char *str)
 {
   int result;
