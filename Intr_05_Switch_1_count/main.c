5ff71c514c0a873f520792f534cc14ca2b17ab41
NesarkarKushal <kushalnesarkar@gmail.com>
2022-05-27 14:55:23 +0530

    Switch 1 Count

---
+++ Intr_05_Switch_1_count/main.c
@@ -0,0 +1,33 @@
+#include<stdint.h>
+#include<stdlib.h>
+
+#include<stdbool.h>
+#include"driverlib/sysctl.h"
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+
+int main(void)
+{
+    int count=0;
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_XTAL_16MHZ|SYSCTL_USE_PLL | SYSCTL_OSC_MAIN);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE,GPIO_PIN_4);
+    GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_4,GPIO_STRENGTH_8MA,GPIO_PIN_TYPE_STD_WPU);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE , GPIO_PIN_1|GPIO_PIN_2| GPIO_PIN_3);//0X0E
+
+    while(1)
+    {
+        if(!GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4))
+        {
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2| GPIO_PIN_3, 0x02);
+            count++;
+        }
+        else
+        {
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2| GPIO_PIN_3, 0x00);
+
+        }
+    }
+}
