6d35135c34b93af82902c0384aad63c4ccea676e
NesarkarKushal <86586956+NesarkarKushal@users.noreply.github.com>
2022-06-12 16:40:36 +0530

    Digital Clock using Timer

--- Digital_clock/main.c
+++ Digital_clock/main.c
@@ -7,12 +7,8 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
-//#include "LCD.h"
-int i=0;
 
-uint32_t avg=0;
-//uint64_t one_min=2400000000;/* Freq=40Mhz ,Time delay-->1sec=1*40Mhz therefore
-                          //    1min=60sec & 60sec delay---> 60*40Mhz=2400000000*/
+int i=0;
 uint64_t oneflag=0;
 uint64_t onesec=40000000;
 uint32_t mins=0;
@@ -23,19 +19,6 @@ void Timer0IntHandler(void);
 int main(void)
 {
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-
-//    LCD_init();
-//    LCD_Clear(); // Clear the Screen
-//    SysCtlDelay(13333333);
-//
-//    LCD_Command(0x0E); //Display ON, Cursor blinking
-//    SysCtlDelay(13333333);
-//
-//    LCD_Clear(); // Clear the Screen
-//    SysCtlDelay(13333333);
-
-
-
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
@@ -62,25 +45,16 @@ void Timer0IntHandler(void)
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 
     oneflag+=1;
-    //print seconds
-//    LCD_cursorPosition(uint8_t 1,uint8_t 2);
-//    Display_Digits(oneflag); // Display Integer digits
 
     if(oneflag==59)
     {
         mins+=1;
         oneflag=0;
-//        LCD_cursorPosition(1,2);// Cursor position
-//        Display_Digits(mins); // Display Integer digits
-//        SysCtlDelay(13333333);
     }
     else if(mins==60)
     {
         mins=0;
         hour+=1;
-//        LCD_cursorPosition(1,2);// Cursor position
-//        Display_Digits(hour); // Display Integer digits
-//        SysCtlDelay(13333333);
     }
 
 }
